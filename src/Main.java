8a13fc36c42ec287c178d2c72dad3e5e9ace099d
Рияз <riaz.karimullin@yandex.ru>
2022-12-02 16:39:00 +0300

    Условный оператор. Часть 1. ДЗ 3

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,55 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задача 1");
+        int userAge = 18;
+        if (userAge >= 2 && userAge <= 6){
+            System.out.println("Если возраст человека равен " + userAge + ", то ему нужно ходить в детский сад");
+        }
+        else{
+            if (userAge >= 7 && userAge <= 18){
+                System.out.println("Если возраст человека равен " + userAge + ", то ему нужно ходить в школу");
+            }
+            else{
+                if (userAge > 18 && userAge < 24){
+                    System.out.println("Если возраст человека равен " + userAge + ", то ему нужно ходить в университет");
+                }
+                else{
+                    System.out.println("Если возраст человека равен " + userAge + ", то ему нужно ходить на работу");
+                }
+            }
+        }
+        System.out.println("Задача 2");
+        int age = 14;
+        if (age < 5){
+            System.out.println("Вы не можете кататься на аттракционе");
+        }
+        else{
+            if (age < 14){
+                System.out.println("Вы можете кататься на аттракционе, только в сопровождении взрослого");
+            }
+            else{
+                System.out.println("Вы можете кататься без сопровождения взрослого");
+            }
+        }
+
+        System.out.println("Задача 3");
+        int one = 14;
+        int two = 8;
+        int three = 12;
+        if (one > two){
+            if (one > three){
+                System.out.println(one);
+            }
+            else{
+                System.out.println(three);
+            }
+        }
+        else {
+            if (two > three) {
+                System.out.println(two);
+            } else {
+                System.out.println(three);
+            }
+        }
     }
 }
